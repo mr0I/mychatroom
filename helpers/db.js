ef5bf5ccc1ff21b4bc69c7bb1babec1a15a9296b
ali <wizard2070@gmail.com>
2022-07-06 12:29:44 +0430

    publish_to_heroku_v5

--- helpers/db.js
+++ helpers/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/socket-db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socket-db');
 const db = mongoose.connection
     .once('open',()=>{console.log('mongo connected:)')})
     .on('error', (error) => {console.warn(error)});
